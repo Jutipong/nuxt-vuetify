8717d18ae10a95a3347cd1542c37868c1a0c04de
js.dev <techcom.ec20@gmail.com>
2024-02-01 16:58:59 +0700

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- <v-app>
+    <LayoutHeader />
+    <LayoutNavigation />
+    <v-main> -->
+  <!-- <v-container fluid> -->
+  <slot></slot>
+  <!-- </v-container> -->
+  <!-- </v-main>
+  </v-app> -->
+</template>
+
+
+
+<!-- <template>
+  <v-app>
+    <LayoutsHeader />
+    <LayoutsNavigation />
+    <v-main>
+      <v-container>
+        <router-view v-slot="{ Component }">
+          <transition
+            name="fade"
+            mode="out-in"
+          >
+            <component :is="Component" />
+          </transition>
+        </router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+  .v-label {
+  color: rgb(var(--v-theme-primary));
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style> -->
---
+++ layouts/empty.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <!-- <v-app>
+    <RouterView /> -->
+  <slot></slot>
+  <!-- </v-app> -->
+</template>
