36d508ce082658c528f70f5203257e7af0f6603c
jutipong subin <T660046@tcrbank.com>
2024-02-01 23:05:22 +0700

    Enable SSR and add keepalive option to app configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   app: {
+    keepalive: true,
     pageTransition: false,
     layoutTransition: false
   },
