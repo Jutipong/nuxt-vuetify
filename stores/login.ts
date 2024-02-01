3b03b2cd4b11397c12572fce1ca8e04f03015424
jutipong subin <T660046@tcrbank.com>
2024-02-01 21:42:41 +0700

    Remove unused import and debugger statement in login store

--- stores/login.ts
+++ stores/login.ts
@@ -1,6 +1,4 @@
 import type { UserLogin } from '@/types/auth'
-// import router from '@/@core/plugins/router'
-
 const { setToken, setUserLogin } = useLocalStorages
 
 export const useLoginStore = defineStore('login', () => {
@@ -21,7 +19,6 @@ export const useLoginStore = defineStore('login', () => {
 
   async function logOut(alertConfirm: boolean = true): Promise<void> {
 
-    debugger;
     if (alertConfirm) {
       if (!await vConfirm.info('Logout!', 'Are you sure you want to log out?', {
         iconTitle: 'mdi-logout',
