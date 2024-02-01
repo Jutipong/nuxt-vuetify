ae746f6f8894697656b5125caafce7447b9c7198
jutipong subin <T660046@tcrbank.com>
2024-02-02 00:25:48 +0700

    Refactor local storage configuration

--- utils/localStorages.ts
+++ utils/localStorages.ts
@@ -1,31 +1,24 @@
 import ls from 'localstorage-slim'
 import type { UserLogin } from '@/types/auth'
 
+ls.config.storage = sessionStorage
+ls.config.encrypt = true
+
 export const useLocalStorages = {
   clearLocalStorage: () => {
-    ls.config.storage = sessionStorage
-    ls.config.encrypt = true
     ls.clear()
   },
   setToken: (token: string) => {
-    ls.config.storage = sessionStorage
-    ls.config.encrypt = true
     ls.set(_const.app.tokenKey, token)
   },
   getToken: (): string | null => {
-    ls.config.storage = sessionStorage
-    ls.config.encrypt = true
     const token = ls.get<string | null>(_const.app.tokenKey)
     return token
   },
   setUserLogin: (userInfo: UserLogin) => {
-    ls.config.storage = sessionStorage
-    ls.config.encrypt = true
     ls.set(_const.app.userLoginKey, userInfo)
   },
   getUserLogin: (): UserLogin | null => {
-    ls.config.storage = sessionStorage
-    ls.config.encrypt = true
     const userInfo = ls.get<UserLogin | null>(_const.app.userLoginKey)
     return userInfo
   },
